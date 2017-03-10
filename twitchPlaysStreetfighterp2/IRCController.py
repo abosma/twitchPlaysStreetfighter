a2500f00a54f3fa7dcbe9b9d44aff4a46e605199
BuildTools <unconfigured@null.spigotmc.org>
2017-03-10 17:29:32 +0100

    whoops, fixed it

---
+++ twitchPlaysStreetfighterp2/IRCController.py
@@ -0,0 +1,62 @@
+import socket, string, time
+import asyncio
+import movesController as mc
+
+
+class IRCController():
+    def __init__(self):
+        self.HOST = "irc.twitch.tv"
+        self.PORT = 6667
+        self.NICK = "aetbosma"
+        self.PASS = 'oauth:m6dceh4p3e46lfhdeavz1x2rxknh95'
+        self.s = socket.socket();
+        self.paused = False;
+
+    def send_message(self, message):
+        self.s.send(bytes("PRIVMSG #atillabosmathesecond :" + message + "\r\n", "UTF-8"))
+   
+    async def bot(self):
+        self.s.connect((self.HOST, self.PORT));
+        self.s.send(bytes("PASS " + self.PASS + "\r\n", "UTF-8"))
+        self.s.send(bytes("NICK " + self.NICK + "\r\n", "UTF-8"))
+        self.s.send(bytes("JOIN #atillabosmathesecond \r\n", "UTF-8"))
+        while True:
+            line = str(self.s.recv(1024))
+            if "End of /NAMES list" in line:
+                break
+
+        while True:
+            for line in str(self.s.recv(1024)).split('\\r\\n'):
+                parts = line.split(':')
+                if len(parts) < 3:
+                    continue
+
+                if "QUIT" not in parts[1] and "JOIN" not in parts[1] and "PART" not in parts[1]:
+                    message = parts[2][:len(parts[2])]
+
+                usernamesplit = parts[1].split("!")
+                username = usernamesplit[0]
+
+                print(username + ": " + message)
+                
+                try:
+                    if not self.paused and username != "aetbosma":
+                        self.send_message(await mc.stringReader(parts[2]));
+                    elif not self.paused and username == "aetbosma":
+                        if message == "pause":
+                            self.paused = True;
+                        else:
+                            self.send_message(await mc.stringReader(parts[2]));
+                    elif self.paused and username != "aetbosma":
+                        self.send_message("Bot is currently paused");
+                    elif self.paused and username == "aetbosma":
+                        if message == "unpause":
+                            self.paused = False;
+                        else:
+                            self.send_message(await mc.stringReader(parts[2]));
+                except Exception as e:
+                    pass
+
+    def startBot(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.bot())
---
+++ twitchPlaysStreetfighterp2/movesController.py
@@ -0,0 +1,501 @@
+import asyncio
+import time
+import re
+import inputController as ic
+
+async def llHadouken():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x64);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x64);
+
+async def rlHadouken():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x64);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x64);
+    
+async def lmHadouken():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x65);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x65);
+
+async def rmHadouken():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x65);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x65);
+
+async def lhHadouken():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x66);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x66);
+
+async def rhHadouken():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x66);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x66);
+
+async def llShoryu():
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x64);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x64);
+
+async def rlShoryu():
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x64);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x64);
+
+async def lmShoryu():
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x65);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x65);
+    
+async def rmShoryu():
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x65);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x65);
+
+async def lhShoryu():
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x66);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x66);
+
+async def rhShoryu():
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x66);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x66);
+
+async def llSenp():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x61);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x61);
+
+async def rlSenp():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x61);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x61);
+    
+async def lmSenp():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x62);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x62);
+
+async def rmSenp():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x62);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x62);
+
+async def lhSenp():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x63);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x63);
+
+async def rhSenp():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x63);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x63);
+
+async def lsuper():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x64);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x64);
+
+async def rsuper():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x64);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x64);
+
+async def lultra():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    ic.PressKey(0x64);
+    ic.PressKey(0x65);
+    ic.PressKey(0x66);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x64);
+    ic.ReleaseKey(0x65);
+    ic.ReleaseKey(0x66);
+
+async def rultra():
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x4B);
+    time.sleep(0.02);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4B);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    ic.PressKey(0x64);
+    ic.PressKey(0x65);
+    ic.PressKey(0x66);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x64);
+    ic.ReleaseKey(0x65);
+    ic.ReleaseKey(0x66);
+
+async def jl():
+    ic.PressKey(0x49);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x49);
+    ic.ReleaseKey(0x4A);
+
+async def jr():
+    ic.PressKey(0x49);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x49);
+    ic.ReleaseKey(0x4C);
+
+async def dl():
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+    time.sleep(0.02);
+    ic.PressKey(0x4A);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4A);
+
+async def dr():
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+    time.sleep(0.02);
+    ic.PressKey(0x4C);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x4C);
+
+async def throw():
+    ic.PressKey(0x61);
+    ic.PressKey(0x64);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x61);
+    ic.ReleaseKey(0x64);
+
+async def focus():
+    ic.PressKey(0x62);
+    ic.PressKey(0x65);
+    time.sleep(1);
+    ic.ReleaseKey(0x62);
+    ic.ReleaseKey(0x65);
+
+async def taunt():
+    ic.PressKey(0x63);
+    ic.PressKey(0x66);
+    time.sleep(0.02);
+    ic.ReleaseKey(0x63);
+    ic.ReleaseKey(0x66);
+
+async def lfadc():
+    ic.PressKey(0x62);
+    ic.PressKey(0x65);
+    time.sleep(0.11);
+    await dl();
+    ic.ReleaseKey(0x62);
+    ic.ReleaseKey(0x65);
+
+async def rfadc():
+    ic.PressKey(0x62);
+    ic.PressKey(0x65);
+    time.sleep(0.11);
+    await dr();
+    ic.ReleaseKey(0x62);
+    ic.ReleaseKey(0x65);
+
+async def lk():
+    ic.PressKey(0x41)
+    time.sleep(0.02);
+    ic.ReleaseKey(0x41);
+
+async def stringReader(moveString):
+    waitTime = 0;
+    builtString = "";
+    for chr in moveString:
+        builtString = builtString + chr;
+        match = re.search('^slp[0-9]{2}$', builtString);
+        if match:
+            timeToWait = int(''.join(list(filter(str.isdigit, match.group(0)))));
+            waitTime = (timeToWait / 1000);
+            builtString = "";
+        if builtString == "llhdk":
+            await llHadouken();
+            builtString = "";
+        if builtString == "rlhdk":
+            await rlHadouken();
+            builtString = "";
+        if builtString == "lmhdk":
+            await lmHadouken();
+            builtString = "";
+        if builtString == "rmhdk":
+            await rmHadouken();
+            builtString = "";
+        if builtString == "lhhdk":
+            await lhHadouken();
+            builtString = "";
+        if builtString == "rhhdk":
+            await rhHadouken();
+            builtString = "";
+        if builtString == "llshy":
+            await llShoryu();
+            builtString = "";
+        if builtString == "rlshy":
+            await rlShoryu();
+            builtString = "";
+        if builtString == "lmshy":
+            await lmShoryu();
+            builtString = "";
+        if builtString == "rmshy":
+            await rmShoryu();
+            builtString = "";
+        if builtString == "lhshy":
+            await lhShoryu();
+            builtString = "";
+        if builtString == "rhshy":
+            await rhShoryu();
+            builtString = "";
+        if builtString == "llsen":
+            await llSenp();
+            builtString = "";
+        if builtString == "rlsen":
+            await rlSenp();
+            builtString = "";
+        if builtString == "lmsen":
+            await lmSenp();
+            builtString = "";
+        if builtString == "rmsen":
+            await rmSenp();
+            builtString = "";
+        if builtString == "lhsen":
+            await lhSenp();
+            builtString = "";
+        if builtString == "rhsen":
+            await rhSenp();
+            builtString = "";
+        if builtString == "lsuper":
+            await lsuper();
+            builtString = "";
+        if builtString == "lultra":
+            await lultra();
+            builtString = "";
+        if builtString == "rsuper":
+            await rsuper();
+            builtString = "";
+        if builtString == "rultra":
+            await rultra();
+            builtString = "";
+        if builtString == "jl":
+            await jl();
+            builtString = "";
+        if builtString == "jr":
+            await jr();
+            builtString = "";
+        if builtString == "dl":
+            await dl();
+            builtString = "";
+        if builtString == "dr":
+            await dr();
+            builtString = "";
+        if builtString == "throw":
+            await throw();
+            builtString = "";
+        if builtString == "focus":
+            await focus();
+            builtString = "";
+        if builtString == "taunt":
+            await taunt();
+            builtString = "";
+        if builtString == "lfadc":
+            await lfadc();
+            builtString = "";
+        if builtString == "rfadc":
+            await rfadc();
+            builtString = "";
+        if builtString == "help":
+            return("Commandlist here: http://pastebin.com/TH3PN3wK");
+        if builtString == "lk":
+            await lk();
+            builtString = "";
+        time.sleep(waitTime);
+    return("Done using moves");
